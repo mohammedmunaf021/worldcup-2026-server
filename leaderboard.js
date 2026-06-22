ce98d756d5a2c0a1296e6e9966be6dd1691f25ff
mohammedmunaf021 <mohammedmunaf21@gmail.com>
2026-06-23 02:06:42 +0530

    Update import paths in leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { db } from "../db.js";
-import { getCache } from "../fixtures.js";
+import { db } from "./db.js";
+import { getCache } from "./fixtures.js";
 
 const router = Router();
 
